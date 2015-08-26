2145078a994f8a5714061d0bba22e27d4272b81b
Samantha Guergenenov <samanthaguergenenov@Presents-MacBook-Pro.local>
2015-08-26 15:12:36 -0400

    final styling

--- taxAngularCtrl.js
+++ taxAngularCtrl.js
@@ -329,10 +329,6 @@ taxCtrl = function($scope){
     transclude: true, 
     link: function(scope, element, attrs) {
       scope.dialogStyle = {};
-      // if (attrs.width)
-      //   scope.dialogStyle.width = attrs.width;
-      // if (attrs.height)
-      //   scope.dialogStyle.height = attrs.height;
       scope.hideModal = function() {
         scope.show = false;
       };
